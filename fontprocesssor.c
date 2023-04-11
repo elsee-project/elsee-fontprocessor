c9c682f4f0382ce984534df7802c1389fd7664d7
shacqc <sc07790@georgiasouthern.edu>
2023-04-10 23:43:42 -0400

    FontProcessor: Fix opening and accessing .tff file; change from direct value access to copying file contents

--- fontprocesssor.c
+++ fontprocesssor.c
@@ -1,19 +1,28 @@
 #include <stdio.h>
 #include <FontMachineRoutines.h>
+#include <FontEngineInit.h>
 
 
-char FontEntry(FILE* FontFile , uint32_t dpi, uint32_t PointSize)
+extern void glyphentry(uint8_t *instructionlist, struct InstructionKit *MemoryLocations, struct glyf *glyf); 
+char FunctionFlags; //1st bit being true means that there is valid data from a previous run, 2nd being true  means a Font Transformation(Ex: Font size change) was requested, 3rd being true means post printer is present, 4th means OS is Windows, 8th bit set means to exit exit
+
+char FontEntry(char filename[255], uint32_t dpi, uint32_t FontSize)    //Only needed input for producing characters from Font File for a display is a TrueType Font file, the dpi on screen and the size of the characters requested 
 {
-    long *ActiveFontEngines;              //In the case why someone asks why I am aggressive with pointers; it is mainly to ensure that seperate data is used in the event that mutliple fonts are used.
-    char FunctionFlags;                         //1st bit being true means that there is valid data from a previous run, 2nd being true  means a Font Transformation(Ex: Font size change) was requested, 3rd being true means post printer is present, 4th means OS is Windows, 8th bit set means to exit exit
-    char *parser = (char)FontFile;
-    checktable(parser);
+    long *ActiveFontEngines;              //In the case why someone asks why I am aggressive with pointers; it is mainly to ensure that seperate data is used in the event that mutliple fonts are used.                             //1st bit being true means that there is valid data from a previous run, 2nd being true means a Font Transformation(Ex: Font size change) was requested, 3rd being true means post printer is present, 4th means OS is Windows, 8th bit set means to exit exit
+    FILE *FontFile = fopen(filename, "rb"); 
+    fseek(FontFile, 0, SEEK_END);
+    int FF_Size = ftell(FontFile);
+    char *parser = (char *)malloc(FF_Size * sizeof(char));
+    rewind(FontFile);
+    fread(parser, FF_Size, 1, FontFile); // Make a copy of Font file
+    fclose(FontFile);  
+    //checktable(parser); TODO: Check if font already processed
     struct FontTable *FontTables = FontDirectory(parser);
-    struct InstructionKit *EngineTools = InitializeFontEngine(parser, FontTables, dpi, PointSize);
+    struct InstructionKit *EngineTools = InitializeFontEngine(parser, FontTables, dpi, FontSize);
     for (int i = 0; i < sizeof(EngineTools -> GlyphCharacterOffsets); i++)
     {
         struct glyf *glyf = glyfsetup(glyf, EngineTools, EngineTools -> GlyphCharacterOffsets[i]);
-        SetPoints(glyf, EngineTools);
+        setPoints(glyf, EngineTools);
         glyphentry(parser, EngineTools, glyf);
         //TO-DO Complete and put Scan command here
 
@@ -21,17 +30,24 @@ char FontEntry(FILE* FontFile , uint32_t dpi, uint32_t PointSize)
     }
 }
 
-char FontChange(FILE* FontFile , uint32_t dpi, uint32_t PointSize)
+char FontChange(char filename[255], uint32_t dpi, uint32_t FontSize)  // Font file, screen dpi and 
 {
-    long (*ActiveFontEngines)[1];              //In the case why someone asks why I am aggressive with pointers; it is mainly to ensure that seperate data is used in the event that mutliple fonts are used.
-    char FunctionFlags;                         //1st bit being true means that there is valid data from a previous run, 2nd being true  means a Font Transformation(Ex: Font size change) was requested, 3rd being true means post printer is present, 4th means OS is Windows, 8th bit set means to exit exit
-    char *parser = (char)FontFile;
+    // long (*ActiveFontEngines)[1];              //In the case why someone asks why I am aggressive with pointers; it is mainly to ensure that seperate data is used in the event that mutliple fonts are used.
+                    
+    FILE *FontFile = fopen(filename, "rb"); 
+    fseek(FontFile, 0, SEEK_END);
+    int FF_Size = ftell(FontFile);
+    char *parser = (char *)malloc(FF_Size * sizeof(char));
+    rewind(FontFile);
+    fread(parser, FF_Size, 1, FontFile); // Make a copy of Font file
+    fclose(FontFile);  
+    //checktable(parser); TODO: Check if font already processed
     struct FontTable *FontTables = FontDirectory(parser);
-    struct InstructionKit *EngineTools = InitializeFontEngine(parser, FontTables, dpi, PointSize);
+    struct InstructionKit *EngineTools = InitializeFontEngine(parser, FontTables, dpi, FontSize);
     for (int i = 0; i < sizeof(EngineTools -> GlyphCharacterOffsets); i++)
     {
         struct glyf *glyf = glyfsetup(glyf, EngineTools, EngineTools -> GlyphCharacterOffsets[i]);
-        SetPoints(glyf, EngineTools);
+        setPoints(glyf, EngineTools);
         int numofGlyphs = 0;
         for(int k = 0; k < sizeof(EngineTools -> ComponentFlags); k++)
         {
@@ -48,24 +64,7 @@ char FontChange(FILE* FontFile , uint32_t dpi, uint32_t PointSize)
     }
 }
 
-char exitFontEngine(struct instructionKit *FontEngine)
+char exitFontEngine(struct InstructionKit *FontEngine)
 {
 free(FontEngine);
 }
-
-int FileReference(FILE* FontEntry, long *ActiveFontEngines)
-    {
-        for (int i = 0; i < sizeof(ActiveFontEngines); i ++)
-        {
-            if (ActiveFontEngines[i] == &FontEntry )
-                {
-                    if (FontEntry == 0)
-                    {
-                        fclose(ActiveFontEngines[i]);
-                        ActiveFontEngines[i] = 0;
-                    }
-
-                }
-             }
-        ActiveFontEngines = malloc(sizeof(ActiveFontEngines)+1);
-    }
